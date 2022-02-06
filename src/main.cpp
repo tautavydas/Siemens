543c9183dc75c93ae55b641c066bd27287b398ee
augustus <tautavydas@yahoo.com>
2022-02-06 18:22:51 +0100

    Producer-Consumer

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]) {
   std::mutex frame_access;
   std::condition_variable wait_condition;
   bool frame_ready{false};
+
   CProducer const Producer(frame_access, wait_condition, frame_ready, frame);
   CConsumer const Consumer(frame_access, wait_condition, frame_ready, frame);
 
